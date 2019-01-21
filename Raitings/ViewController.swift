c0866f14bbb528496370d62a951f045d894c1ca5
Dima Kohut <masterullo@gmail.com>
2019-01-22 00:44:17 +0200

    Initial Commit

---
+++ Raitings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Raitings
+//
+//  Created by DmytroKohut on 1/22/19.
+//  Copyright © 2019 DmytroKohut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
